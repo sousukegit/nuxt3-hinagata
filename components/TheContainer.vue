aa420fbf167aefb27b5046997a5d49bca26781d1
sousukegit <greenneosoul.sousuke@gmail.com>
2023-12-16 10:23:55 +0900

    トップページ完成

--- components/TheContainer.vue
+++ components/TheContainer.vue
@@ -2,9 +2,7 @@
 <!-- 共通機能 -->
 <div :class="mode">
     <div class="min-h-screen bg bg-main-300 py-2 px-4 text-coffee dark:bg-coffee dark:text-main-100">
-        <div>
-            <h1>DB登録アプリ</h1> 
-        </div>
+
         <div class="flex justify-between">
             <Thebreadcrumbs/>
             <ModeSwitcher :dark-or-empty="mode" :switch-mode="switchMode"></ModeSwitcher>
@@ -12,6 +10,7 @@
         </div>
 <!-- コンテンツゾーン -->
         <div class="container mx-auto mt-4 rounded-md bg-main-50 p-4 dark:border-2 dark:bg-coffee dark:shadow-2xl dark:shadow-sub-200">
+            
             <slot/>  
         </div>
 <!-- 枠外下 -->
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,27 @@
 <template>
-  
-    
       <TheContainer >
-        <div :class="mode">
+        <AppJumbotron><h1>私のポートフォリオ</h1></AppJumbotron>
+        <div class="grid grid-cols-1 sm:grid-cols-2">
+          <div>
+            <AppH2>このサイトについて</AppH2>
+            <p>このサイトは私のポートフォリオです。</p>
+            <p>NuxtとTailwindで作成しています。</p>
+          </div>
+          <div class="">
+            <AppH2>メニュー</AppH2>
+            <AppLink :href="a">プロフィール</AppLink>
+            <AppLink :href="a">作品</AppLink>
+          </div>
+
+        </div>
+
     
-      <div class="dark:bg-coffee">
-
-      
-    
-  
-    <div class="bg-emerald-200 dark:bg-emerald-900 dark:text-white" >sousuek</div>
-
-    <div class="bg-blue-400 dark:bg-blue-700 dark:text-white" >hogehogehgoe</div>
-      <form action="">
-
-        <InputText v-model="text" ></InputText>
-        <InputNum v-model="num" ></InputNum>
-        <InputTextarea v-model="text" ></InputTextarea>
-        <div>tekiduto{{ text }}</div>
-      </form>
-
-      </div>
-      </div>
       </TheContainer>
   
 </template>
 
 <script setup lang="ts">
-//ダークかライトか切り替え用のrefを用意
-const mode = useCookie("mode",{maxAge:60*60*24*365,})
-const switchMode = () =>{
-  mode.value =mode.value? "" :"dark";
-  
-}
+
 
 
 
